8dae3a272e64fc7991388364029896942535fcbd
Barış Kaynakdemir <65814113+thatsarerealstonehorseimriding@users.noreply.github.com>
2022-08-05 20:19:18 +0300

    Add files via upload

---
+++ ProductDal.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdoNetDemo
+{
+    public class ProductDal
+    {
+        SqlConnection _connection = new SqlConnection(@"server=(localdb)\mssqllocaldb;initial catalog=ETrade;integrated security=true");
+        public List<Product> GetAll()
+        {
+            ConnectionControl();
+            SqlCommand command = new SqlCommand("Select * from Products", _connection);
+
+            SqlDataReader reader = command.ExecuteReader();
+
+            List<Product> products = new List<Product>();
+            while (reader.Read())
+            {
+                Product product = new Product
+                {
+                    Id = Convert.ToInt32(reader["ID"]),
+                    Name = reader["Name"].ToString(),
+                    StockAmount = Convert.ToInt32(reader["StockAmount"]),
+                    UnitPrice = Convert.ToDecimal(reader["UnitPrice"])
+                };
+                products.Add(product);
+            }
+            reader.Close();
+            _connection.Close();
+            return products;
+
+        }
+
+        private void ConnectionControl()
+        {
+            if (_connection.State == ConnectionState.Closed)
+            {
+                _connection.Open();
+            }
+        }
+
+        public void Add(Product product)
+        {
+            ConnectionControl();
+            SqlCommand command = new SqlCommand(
+                "Insert into Products values(@name,@unitPrice,@stockAmount)",_connection);
+             command.Parameters.AddWithValue("@Name",product.Name);
+             command.Parameters.AddWithValue("@unitPrice", product.UnitPrice);
+            command.Parameters.AddWithValue("@stockAmount", product.StockAmount);
+
+            command.ExecuteNonQuery();
+            _connection.Close();
+
+        }
+    }
+}
